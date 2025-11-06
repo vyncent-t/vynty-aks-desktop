c9e0193923f7f06b4e173c3af1a49ae8b7019a8b
Oleksandr Dubenko <oldubenko@microsoft.com>
2025-11-06 13:14:55 -0500

    upstreamable: Use location.reload when deleting cluster

--- frontend/src/components/App/Home/ClusterContextMenu.tsx
+++ frontend/src/components/App/Home/ClusterContextMenu.tsx
@@ -73,7 +73,7 @@ export default function ClusterContextMenu({ cluster }: ClusterContextMenuProps)
         }
       })
       .finally(() => {
-        history.push('/');
+        window.location.reload();
       });
   }
 
