020e5fe5db3a47dea8f8406a5d9f075e2a841971
René Dudfield <renedudfield@microsoft.com>
2025-12-26 10:09:44 -0300

    aks-plugin: theme: Fix import path for AppTheme
    Also add some tsignore todos for some fields which are used but
    do not exist in theme AppTheme.

--- plugins/aks-desktop/src/utils/shared/theme.ts
+++ plugins/aks-desktop/src/utils/shared/theme.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the Apache 2.0.
 
-import { AppTheme } from '@kinvolk/headlamp-plugin/lib';
+import type { AppTheme } from '@kinvolk/headlamp-plugin/lib/AppTheme';
 
 export const azureTheme: AppTheme = {
   name: 'Azure Theme',
@@ -10,10 +10,12 @@ export const azureTheme: AppTheme = {
   secondary: '#ecebe9', // Gray neutral color
   text: {
     primary: '#000000', // Black for maximum contrast and readability
+    // @ts-ignore todo: fix palette type so text.secondary is recognized
     secondary: '#323130', // Dark gray for secondary text
   },
   background: {
     default: '#ffffff', // Off-white for main background
+    // @ts-ignore todo: fix palette type so background.paper is recognized
     paper: '#ffffff', // White for cards/panels
     muted: '#f0f0f0', // Light gray for muted backgrounds
   },
