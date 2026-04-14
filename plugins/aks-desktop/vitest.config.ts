53ad4fba365adbad3d8ce03bbda9ff49ef412d06
Evangelos Skopelitis <eskopelitis@microsoft.com>
2026-04-14 12:41:55 -0400

    aks-desktop: Split unit and a11y tests
    Signed-off-by: Evangelos Skopelitis <eskopelitis@microsoft.com>
    
    change 2

--- plugins/aks-desktop/vitest.config.ts
+++ plugins/aks-desktop/vitest.config.ts
@@ -4,8 +4,26 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: 'node_modules/@kinvolk/headlamp-plugin/config/setupTests.js',
     clearMocks: true,
+    projects: [
+      {
+        test: {
+          name: 'unit',
+          environment: 'jsdom',
+          setupFiles: 'node_modules/@kinvolk/headlamp-plugin/config/setupTests.js',
+          include: ['src/**/*.test.{ts,tsx}'],
+          exclude: ['src/**/*.guidepup.test.tsx'],
+        },
+      },
+      {
+        test: {
+          name: 'a11y',
+          environment: 'jsdom',
+          setupFiles: 'node_modules/@kinvolk/headlamp-plugin/config/setupTests.js',
+          include: ['src/**/*.guidepup.test.tsx'],
+          testTimeout: 30000,
+        },
+      },
+    ],
   },
 });
